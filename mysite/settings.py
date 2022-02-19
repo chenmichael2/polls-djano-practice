2cec4d65ffadf40e2b358c449d2fa7445937b903
chenmichael2 <chenmichael2012@gmail.com>
2022-02-18 21:03:31 -0800

    commiting database

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'polls_practice',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'OPTIONS': {
+#             'service': 'my_service',
+#             'passfile': '.my_pgpass',
+#         },
+#     }
+# }
 
 
 # Password validation
